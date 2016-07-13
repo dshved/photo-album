dd43fc6bf10de40ec69f09d5fa2a160527ce8cb0
Dmitry Shved <dshved@hotmail.com>
2016-07-13 15:54:24 +0300

    Update 2016-07-13T12:54:15.252Z

--- gulp/paths/js.foundation.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = [
-	'./bower_components/jquery/dist/jquery.min.js',
-	'./bower_components/highlightjs/highlight.pack.min.js',
-	'./bower_components/svg4everybody/dist/svg4everybody.js'
-];
